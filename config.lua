367a44b3a0b5a126bc766fee9e3e9b9cb237f9ef
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2021-08-15 22:43:23 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -16,3 +16,9 @@ Config.DownHealth = 130
 
 -- How low health for player to die.
 Config.DeadHealth = 2
+
+-- Time to hold E to revive
+Config.ReviveTime = 3
+
+-- Time to hold R to respawn
+Config.RespawnTime = 3
